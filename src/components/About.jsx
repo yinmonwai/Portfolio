0bdc11bc442bc8f1b051393a42ef8c2db5f5dc38
yinmonwai <yinmonwai078@gmail.com>
2025-09-28 20:26:28 +0630

    prepare my portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
         </Typography>
         <Typography variant="body1" sx={{ maxWidth: "100%" }}>
           I’m a Full Stack Web Developer with experience in building responsive
-          and scalable web applications. Highly skilled and dedicated web developer with a strong background in HTML, CSS,Bootstrap,Tailwindcss and JavaScript. Proficient in Oracle SQL and experienced in PHP programming ,Laravel, Livewire and MySql. Possess extensive knowledge of ReactJS,MUI ,Ant design,shadcn,Next.js ,node.js and Mongodb.Knowledge of Postman to test Backend API. Committed to delivering top-quality web solutions and providing exceptional web development and version control system git, github.Knowledge of Microsoft team,bitbucket and Click Up.Collaboration with Other teams.I enjoy solving problems and creating clean, maintainable code. My focus is on delivering modern, user-friendly solutions that work across devices.
+          and scalable web applications. Highly skilled and dedicated web developer with a strong background in HTML, CSS,Bootstrap,Tailwindcss and JavaScript. Proficient in Oracle SQL and experienced in PHP programming ,Laravel, Livewire,MySql and laravel with api. Possess extensive knowledge of ReactJS,MUI ,Ant design,shadcn,Next.js ,node.js and Mongodb.Knowledge of Postman to test Backend API. Committed to delivering top-quality web solutions and providing exceptional web development and version control system git, github.Knowledge of Microsoft team,bitbucket and Click Up.Collaboration with Other teams.I enjoy solving problems and creating clean, maintainable code. My focus is on delivering modern, user-friendly solutions that work across devices.
         </Typography>
       </Container>
     </Box>
